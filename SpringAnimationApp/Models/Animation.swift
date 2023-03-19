bfe118b1606cc85fbd81cfaf68b0aea86127b423
Ilya Zemskov <zemskov.ilya@gmail.com>
2023-03-19 18:54:37 -0300

    Model changed

--- SpringAnimationApp/Models/Animation.swift
+++ SpringAnimationApp/Models/Animation.swift
@@ -13,13 +13,23 @@ struct Animation {
     let force: Double
     let duration: Double
     let delay: Double
+    
+    var description: String {
+        """
+        Preset: \(preset)
+        Curve: \(curve)
+        Force: \(String(format: "%.2f", force))
+        Duration: \(String(format: "%.2f", duration))
+        Delay: \(String(format: "%.2f", delay))
+        """
+    }
 }
 
 extension Animation {
     static func getAnimation() -> Animation {
         Animation(
-            preset: DataSource.shared.preset.randomElement() ?? "nil",
-            curve: DataSource.shared.curve.randomElement() ?? "nil",
+            preset: DataSource.shared.preset.randomElement()?.rawValue ?? "pop",
+            curve: DataSource.shared.curve.randomElement()?.rawValue ?? "easeIn",
             force: Double.random(in: 1.00...2.50),
             duration: Double.random(in: 1.00...3.00),
             delay: Double.random(in: 0.15...0.40)
