b1c907b283fff28c9d220998987efd2b854ab26e
Ilya Zemskov <zemskov.ilya@gmail.com>
2023-03-19 19:05:24 -0300

    Re-wrote logic for SpringAnimationVC

--- SpringAnimationApp/ViewControllers/SpringAnimationViewController.swift
+++ SpringAnimationApp/ViewControllers/SpringAnimationViewController.swift
@@ -8,72 +8,34 @@
 import UIKit
 import SpringAnimation
 
-
-enum AnimationStatus {
-    case current, next
-}
-
 final class SpringAnimationViewController: UIViewController {
     
-    // MARK: -IBOutlets
+    // MARK: - IBOutlets
     @IBOutlet var springAnimationView: SpringView!
     @IBOutlet var animationLabel: UILabel!
-    @IBOutlet var runButton: UIButton!
     
-    private var animations: [Animation] = []
-    private var animationIndex = 0
-    private var runningAnimation = AnimationStatus.current
+    private var animation = Animation.getAnimation()
     
     //MARK: - ViewLifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        setAnimation()
         setAnimationDescription()
     }
     
-    @IBAction func runButtonTapped() {
-        switch runningAnimation {
-        case .current:
-            setAnimationDescription()
-            setAnimationParameters()
-            animationIndex += 1
-            runButton.setTitle("Run \(animations[animationIndex].preset)", for: .normal)
-            runningAnimation = .next
-        case .next:
-            setAnimationDescription()
-            setAnimationParameters()
-            setAnimation()
-            animationIndex -= 1
-            runButton.setTitle("Run \(animations[animationIndex].preset)", for: .normal)
-            runningAnimation = .current
-        }
+    @IBAction func runButtonTapped(_ sender: UIButton) {
+        setAnimationDescription()
+        springAnimationView.animation = animation.preset
+        springAnimationView.curve = animation.curve
+        springAnimationView.force = animation.force
+        springAnimationView.duration = animation.duration
+        springAnimationView.delay = animation.delay
+        springAnimationView.animate()
+        animation = Animation.getAnimation()
+        sender.setTitle("Run \(animation.preset)", for: .normal)
     }
     
     // MARK: - Animation description and settings
-    private func setAnimation() {
-        animations = []
-        for _ in 0...1 {
-            let animation = Animation.getAnimation()
-            animations.append(animation)
-        }
-    }
-    
     private func setAnimationDescription() {
-        animationLabel.text = """
-                            Preset: \(animations[animationIndex].preset)
-                            Curve: \(animations[animationIndex].curve)
-                            Force: \(String(format: "%.2f", animations[animationIndex].force))
-                            Duration: \(String(format: "%.2f", animations[animationIndex].duration))
-                            Delay: \(String(format: "%.2f", animations[animationIndex].delay))
-                            """
-    }
-    
-    private func setAnimationParameters() {
-        springAnimationView.animation = animations[animationIndex].preset
-        springAnimationView.curve = animations[animationIndex].curve
-        springAnimationView.force = animations[animationIndex].force
-        springAnimationView.duration = animations[animationIndex].duration
-        springAnimationView.delay = animations[animationIndex].delay
-        springAnimationView.animate()
+        animationLabel.text = animation.description
     }
 }
