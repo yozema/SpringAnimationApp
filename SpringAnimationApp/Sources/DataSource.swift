19a5b7be270ff486bd5d2f67836647032e1ef45e
Ilya Zemskov <zemskov.ilya@gmail.com>
2023-03-19 18:43:35 -0300

    Data source changed

--- SpringAnimationApp/Sources/DataSource.swift
+++ SpringAnimationApp/Sources/DataSource.swift
@@ -6,72 +6,15 @@
 //
 
 import Foundation
+import SpringAnimation
 
 final class DataSource {
     
     static let shared = DataSource()
     
-    let preset = [
-        "pop",
-        "slideLeft",
-        "slideRight",
-        "slideDown",
-        "slideUp",
-        "squeezeLeft",
-        "squeezeRight",
-        "squeezeDown",
-        "squeezeUp",
-        "fadeIn",
-        "fadeOut",
-        "fadeOutIn",
-        "fadeInLeft",
-        "fadeInRight",
-        "fadeInDown",
-        "fadeInUp",
-        "zoomIn",
-        "zoomOut",
-        "fall",
-        "shake",
-        "flipX",
-        "flipY",
-        "morph",
-        "squeeze",
-        "flash",
-        "wobble",
-        "swing"
-    ]
+    let preset = AnimationPreset.allCases
     
-    let curve = [
-        "easeIn",
-        "easeOut",
-        "easeInOut",
-        "linear",
-        "spring",
-        "easeInSine",
-        "easeOutSine",
-        "easeInOutSine",
-        "easeInQuad",
-        "easeOutQuad",
-        "easeInOutQuad",
-        "easeInCubic",
-        "easeOutCubic",
-        "easeInOutCubic",
-        "easeInQuart",
-        "easeOutQuart",
-        "easeInOutQuart",
-        "easeInQuint",
-        "easeOutQuint",
-        "easeInOutQuint",
-        "easeInExpo",
-        "easeOutExpo",
-        "easeInOutExpo",
-        "easeInCirc",
-        "easeOutCirc",
-        "easeInOutCirc",
-        "easeInBack",
-        "easeOutBack",
-        "easeInOutBack"
-    ]
+    let curve = AnimationPreset.allCases
     
     private init() {}
 }
